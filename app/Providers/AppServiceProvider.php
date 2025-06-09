7ebf0cbc80532d9e01af00f960dc48b81c914999
unknown <ahmadferdi629@gmail.com>
2025-06-09 08:49:31 -0700

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         
+         if($this->app->environment('production')) {
+        \URL::forceScheme('https');
+    }
         View::composer('*', function ($view) {
             $view->with('settings', Setting::getMultiple([
                 'contact_address',
